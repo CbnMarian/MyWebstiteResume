7fa49d3d18ce67bc9212b1c63e1286376fb2f8ea
CbnMarian <m.marian03@yahoo.com>
2023-11-20 14:50:05 +0200

    1

--- src/scripts/projects/joke-robot/jokes.js
+++ src/scripts/projects/joke-robot/jokes.js
@@ -1,4 +1,4 @@
-const button = document.getElementById("button-robot");
+/* const button = document.getElementById("button-robot");
 const audioElement = document.getElementById("audio-robot");
 
 //disable/enable button
@@ -47,3 +47,4 @@ async function getJokes() {
 
 button.addEventListener("click", getJokes);
 audioElement.addEventListener("ended", toggleButton);
+ */
